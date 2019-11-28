b50a2be86f4174003ceebc3628f665eefb599233
adoxic <griffinmaeve@gmail.com>
2019-11-28 10:56:43 -0800

    started on state

--- src/components/Breath.js
+++ src/components/Breath.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+const breath1 = '../images/breath1.png';
+const breath2 = '../images/breath2.png';
+const breath3 = '../images/breath3.png';
+const breath4 = '../images/breath4.png';
 
 const Breath = () => {
+  const [breath, setBreath] = useState(); 
+  useEffect(() => {
+    setBreath(breath1);
+  }, [breath]);
+
   return (
     <section>
-      <img src={'../images/breath1.png'} />
+      <img src={breath} />
     </section>
   );
 };
